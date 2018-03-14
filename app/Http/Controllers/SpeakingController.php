ad57cb03e8cac1bb65ba8054f9593a05311bdf34
shockX92 <fabien.roussel921@gmail.com>
2018-03-14 19:51:14 +0100

    Suppress personal data from the controler

--- app/Http/Controllers/SpeakingController.php
+++ app/Http/Controllers/SpeakingController.php
@@ -18,25 +18,28 @@ class SpeakingController extends Controller {
      */
     public function speakPost(Request $request) {
         $input = $request->all();
+        //check input
         if (!isset($input['text']) ) {
+            return response()->json(array(
+                        'code' => '400',
+                        'msg'  => 'BAD REQUEST : Missing field text in Speaking Controller'
+            ));
             throw new \InvalidArgumentException('Missing the required parameter $text when calling speakPost');
         }
         if (!isset($input['language']) ) {
             $language = 'fr-FR_ReneeVoice';
+        }
+        else if ($input['language'] == "en-US_AllisonVoice" ) {
+            $language = 'en-US_AllisonVoice';
         }
         else
             $language = ($input['language']);
         
         $credentials = CoreUtils::loadCredentials();
 
-/// this username and password are in resources/credentials/surirobotTTS.json 
-    //but I don't know how to access the directory resources
-        //$username = 'cbe6f058-0b37-4e9d-83a2-d8da15a04cf4';
-        //$password = 'TWi3NCHBh4nO';
         $headers = array( 'Content-Type: application/json',
                           'Accept: audio/wav');
-
-        //$filepath = CoreUtils::saveFile($data_json); 
+        
         $data_json = json_decode($input['text'], true); 
         $postdata = array(
             'accept'    => 'audio/wav',
@@ -51,23 +54,31 @@ class SpeakingController extends Controller {
         curl_setopt($curlPost, CURLOPT_USERPWD, $credentials->username . ':' . $credentials->password);
         curl_setopt($curlPost, CURLOPT_HTTPHEADER, $headers); 
         curl_setopt($curlPost, CURLOPT_POSTFIELDS, $data_json); //"{\"text\":\"hello world\"}"
+        // retrieve the answer from the API
         $response  = curl_exec($curlPost);
         curl_close($curlPost);
-
+        
+        //transcript the json into an array
         $data_json = json_decode($response, true); 
+        //check the code if we encounter an error
         if ($data_json['code']>299 ) {
             return response()->json(array(
                         'code' => $data_json['code'],
                         'msg'  => $data_json['error']
             ));
         }
+        // Creating file 
         $filePathName = './storage/' . uniqid() . '-Speaking.wav';
+        //if we did not succeed to creat it
         if(!file_put_contents($filePathName, $response)){
             return response()->json(array(
                         'code' => '300',
                         'msg'  => 'Error putting file'
             ));
         }
+        
+        
+        // if it encountered no problem
         return response()->json(array(
                     'code' => 200,
                     'msg'  => 'OK',
@@ -77,96 +88,3 @@ class SpeakingController extends Controller {
     }
 
 }
-
-/*
-
-public function speakPost(Request $request) {
-        $input = $request->all();
-        if (!isset($input['text']) || !isset($input['nameFile'])) {
-            throw new \InvalidArgumentException('Missing the required parameter $text when calling speakPost');
-        }
-
-/// this username and password are in resources/credentials/surirobotTTS.json 
-    //but I don't know how to access the directory resources
-        $username = 'cbe6f058-0b37-4e9d-83a2-d8da15a04cf4';
-        $password = 'TWi3NCHBh4nO';
-        $headers = array( 'Content-Type: application/json',
-                          'Accept: audio/wav');
-        $data_json = json_decode($input['text']);    
-        //or
-        //$data_json = = $input['text'];
-
-        //$filepath = CoreUtils::saveFile($data_json); 
-
-        $postdata = array(
-            'accept'    => 'audio/wav',
-            'text'      => $data_json->{'text'}
-        );
-
-        $curlPost = curl_init();
-        curl_setopt($curlPost, CURLOPT_POST, 1);
-        curl_setopt($curlPost, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($curlPost, CURLOPT_URL, "https://stream.watsonplatform.net/text-to-speech/api/v1/synthesize?voice=fr-FR_ReneeVoice"); //en-US_AllisonVoice
-        curl_setopt($curlPost, CURLOPT_USERPWD, $username.':'.$password);
-        curl_setopt($curlPost, CURLOPT_HTTPHEADER, $headers); 
-        curl_setopt($curlPost, CURLOPT_POSTFIELDS, $postdata); //"{\"text\":\"hello world\"}"
-        $response  = curl_exec($curlPost);
-        curl_close($curlPost);
-
-        file_put_contents('./' . $input['nameFile'] . '.wav', $response);
-        return response()->json(array(
-                    'code' => 200,
-                    'msg'  => 'OK'
-        ));
-    }
-
-    */
-
-
-
-
-/****
-working
-
-
-public function speakPost(Request $request) {
-        $input = $request->all();
-        if (!isset($input['text']) ) {
-            throw new \InvalidArgumentException('Missing the required parameter $text when calling speakPost');
-        }
-
-/// this username and password are in resources/credentials/surirobotTTS.json 
-    //but I don't know how to access the directory resources
-        $username = 'cbe6f058-0b37-4e9d-83a2-d8da15a04cf4';
-        $password = 'TWi3NCHBh4nO';
-        $headers = array( 'Content-Type: application/json',
-                          'Accept: audio/wav');
-
-        //$filepath = CoreUtils::saveFile($data_json); 
-
-        $postdata = array(
-            'accept'    => 'audio/wav',
-            'text'      => $input['text']
-        );
-        $data_json = json_encode($postdata); 
-
-        $curlPost = curl_init();
-        curl_setopt($curlPost, CURLOPT_POST, 1);
-        curl_setopt($curlPost, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($curlPost, CURLOPT_URL, "https://stream.watsonplatform.net/text-to-speech/api/v1/synthesize?voice=fr-FR_ReneeVoice"); //en-US_AllisonVoice
-        curl_setopt($curlPost, CURLOPT_USERPWD, $username.':'.$password);
-        curl_setopt($curlPost, CURLOPT_HTTPHEADER, $headers); 
-        curl_setopt($curlPost, CURLOPT_POSTFIELDS, $data_json); //"{\"text\":\"hello world\"}"
-        $response  = curl_exec($curlPost);
-        curl_close($curlPost);
-
-        file_put_contents('./' . uniqid() . '-Speaking.wav', $response);
-        return response()->json(array(
-                    'code' => 200,
-                    'msg'  => 'OK',
-                    'data' => $input['text']
-        ));
-    }
-
-
-    */
